a03c94298ac48833436e3bcd5beddefe3a283478
Nancy Pineda <new.lifenrpn@gmail.com>
2022-12-21 12:53:29 -0800

    Add files via upload

--- Pewlett-Hackard-Analysis/Queries/Employee_Database_challenge.sql
+++ Pewlett-Hackard-Analysis/Queries/Employee_Database_challenge.sql
@@ -41,3 +41,33 @@ GROUP BY title
 ORDER BY COUNT(title) DESC;
 
 SELECT * FROM retiring_titles; 
+
+-- Deliverable 2:
+
+SELECT DISTINCT ON (e.emp_no)e.emp_no
+    e.first_name,
+    e.last_name,
+    e.birth_date,
+    de.from_date,
+    de.to_date,
+    t.title
+INTO mentorship_program
+FROM employees AS e
+    INNER JOIN dept_emp AS de
+
+SELECT DISTINCT ON(e.emp_no)e.emp_no,
+	e.first_name,
+	e.last_name,
+	e.birth_date,
+	de.from_date,
+	de.to_date,
+	t.title
+INTO mentorship_table
+FROM employees AS e
+	INNER JOIN dept_emp AS de
+		ON (e.emp_no = de.emp_no)
+			INNER JOIN titles AS t
+				ON (e.emp_no = t.emp_no)
+WHERE (e.birth_date BETWEEN '1962-01-01' AND '1965-12-31')
+AND (de.to_date = '9999-01-01')
+ORDER BY emp_no;
